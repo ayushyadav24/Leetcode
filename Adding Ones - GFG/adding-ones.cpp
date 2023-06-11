0b6c71da75d5fd75f713986f94cdaf7592e0f550
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-06-11 19:08:57 +0530

    Added solution - LeetHub

---
+++ Adding Ones - GFG/adding-ones.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    void update(int A[], int n, int updates[], int k)
+    {
+        // Your code goes here
+        for(int i=0; i<k; i++)
+        {
+            int s = updates[i];
+            A[updates[i]-1]++;
+        }
+        int temp = 0;
+        for(int i=0; i<n; i++)
+        {
+            A[i] += temp;
+            temp = A[i];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t; cin>>t;
+	while(t--)
+	{
+		int n,k;
+		cin>>n>>k;
+		int a[n]={0}, updates[k]={0};
+		for(int i = 0; i < k; i++)
+		cin>>updates[i];
+		Solution ob;
+		ob.update(a, n, updates, k);
+		
+		for(int i = 0; i < n; i++)
+		cout<<a[i]<<" ";
+		
+		cout<<endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
