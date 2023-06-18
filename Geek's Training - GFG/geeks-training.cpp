89e517c3b01942f828d71a4f31390f90f90fc008
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-06-18 18:28:28 +0530

    Added solution - LeetHub

--- Geek's Training - GFG/geeks-training.cpp
+++ Geek's Training - GFG/geeks-training.cpp
@@ -5,41 +5,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int func(int day, int last, vector<vector<int>>& points, vector<vector<int>>&dp)
-    {
-        // base case
-        if(day == 0)
-        {
-            int maxi = 0;
-            for(int task=0; task <3; task++)
-            {
-                if(task != last)
-                {
-                    maxi = max(maxi, points[0][task]);
-                }
-            }
-            return maxi;
-        }
-        
-        if(dp[day][last] != -1)
-        {
-            return dp[day][last];
-        }
-        
-        int maxi = 0;
-        for(int task=0; task<3; task++)
-        {
-            if(task != last)
-            {
-                int point = points[day][task] + func(day-1, task, points, dp);
-                maxi = max(maxi, point);
-            }
-        }
-        return dp[day][last] = maxi;
-    }
     int maximumPoints(vector<vector<int>>& points, int n) {
         vector<vector<int>>dp(n, vector<int>(4, -1));
-        return func(n-1, 3, points, dp);
+        dp[0][0] = max(points[0][1], points[0][2]);
+        dp[0][1] = max(points[0][0], points[0][2]);
+        dp[0][2] = max(points[0][0], points[0][1]);
+        dp[0][3] = max(points[0][0], max(points[0][1], points[0][2]));
+        
+        for(int day=1; day <n; day++)
+        {
+            for(int last = 0; last < 4; last++)
+            {
+                dp[day][last] = 0;
+                for(int task = 0; task < 3; task++)
+                {
+                    if(task != last)
+                    {
+                       int point = points[day][task] + dp[day-1][task];
+                       dp[day][last] = max(dp[day][last], point);
+                    }
+                }
+            }
+        }
+        return dp[n-1][3];
     }
 };
 
