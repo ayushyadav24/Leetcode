42c1936e830663ef63d5ce6b39728006b10fb5a7
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-06-20 15:20:20 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -9,34 +9,39 @@ using namespace std;
 
 class Solution{
 public:
-    int helper(int i, int j, vector<vector<int>> &arr, int n, vector<vector<int>> &dp)
+    int maximumPath(int n, vector<vector<int>> arr)
     {
-        // BASE CASES
-        //1 OUT OF BOUND CASE
-        if(j<0 || j>=n)
-            return -1e9;
-        
-        //2 DESTINATION
-        if(i==0)
-            return arr[i][j];
-            
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        
-        // EXPLORE ALL PATHS
-        int s = arr[i][j] + helper(i-1, j, arr, n, dp);
-        int ld = arr[i][j] + helper(i-1, j-1, arr, n, dp);
-        int rd = arr[i][j] + helper(i-1, j+1, arr, n, dp);
-        
-        return dp[i][j] = max(s, max(ld, rd));
-    }
-    int maximumPath(int n, vector<vector<int>> matrix)
-    {
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        int maxi = INT_MIN;
+        vector<vector<int>>dp(n, vector<int>(n, 0));
+        // initialisation
         for(int j=0; j<n; j++)
         {
-            maxi = max(maxi, helper(n-1, j, matrix, n, dp));
+            dp[0][j] = arr[0][j];
+        }
+        
+        for(int i=1; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                int ld, rd;
+                int s = arr[i][j] + dp[i-1][j];
+                ld = arr[i][j];
+                if(j-1>=0)
+                    ld += dp[i-1][j-1];
+                else
+                    ld += -1e9;
+                rd = arr[i][j];
+                if(j+1 < n)
+                    rd += dp[i-1][j+1];
+                else
+                    rd += -1e9;
+                dp[i][j] = max(s, max(ld, rd));
+            }
+        }
+        
+        int maxi = dp[n-1][0];
+        for(int j=1; j<n; j++)
+        {
+            maxi = max(maxi, dp[n-1][j]);
         }
         return maxi;
     }
