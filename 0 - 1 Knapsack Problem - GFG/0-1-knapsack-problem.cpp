75a6708ae95a281a46fdc1722d7558af5f3af1d8
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-06-25 02:13:30 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,39 +8,29 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int func(int ind, int wt[], int val[], int W, vector<vector<int>> &dp)
-    {
-        // base case
-        if(ind==0)
-        {
-            if(W >= wt[0])
-            {
-                return val[0];
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        
-        if(dp[ind][W] != -1)
-            return dp[ind][W];
-        
-        // two option pick not pick
-        int notPick = 0 + func(ind-1, wt, val, W, dp);
-        int pick = INT_MIN;
-        if(W >= wt[ind])
-        {
-            pick = val[ind] + func(ind-1, wt, val, W-wt[ind], dp);
-        }
-        
-        return dp[ind][W] = max(notPick, pick);
-        
-    }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       vector<vector<int>>dp(n, vector<int>(W+1, -1));
-       return func(n-1, wt, val, W, dp);
+       vector<vector<int>>dp(n, vector<int>(W+1, 0));
+       
+       // base case initialisation
+       for(int w = wt[0]; w <= W; w++)
+       {
+           dp[0][w] =  val[0];
+       }
+       
+       for(int i=1; i<n; i++)
+       {
+           for(int w=0; w<= W; w++)
+           {
+               int np = 0 + dp[i-1][w];
+               int p = INT_MIN;
+               if(w >= wt[i])
+                    p = val[i] + dp[i-1][w-wt[i]];
+                dp[i][w] = max(p, np);
+           }
+       }
+       
+       return dp[n-1][W];
     }
 };
 
