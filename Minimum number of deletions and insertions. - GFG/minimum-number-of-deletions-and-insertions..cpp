8554b16e123a707e4040f915d1010226f3596bb7
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-06-28 01:20:28 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -10,14 +10,14 @@ class Solution{
 	int utility(int ind1, int ind2, string s1, string s2, vector<vector<int>> &dp)
 	{
 	    // base case
-	    if(ind1 < 0 || ind2 < 0)
+	    if(ind1 == 0 || ind2 == 0)
 	        return 0;
 	        
 	   if(dp[ind1][ind2] != -1)
 	        return dp[ind1][ind2];
 	       
 	    // char match
-	    if(s1[ind1] == s2[ind2])
+	    if(s1[ind1 - 1] == s2[ind2 - 1])
 	        return dp[ind1][ind2] = 1+utility(ind1-1, ind2-1, s1, s2, dp);
 	   
 	   // char not matching    
@@ -27,8 +27,8 @@ class Solution{
 	{
 	    int n = s1.length();
         int m = s2.length();
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return utility(n-1, m-1, s1, s2, dp);
+        vector<vector<int>>dp(n+1, vector<int>(m+1, ));
+        return utility(n, m, s1, s2, dp);
 	}
 	int minOperations(string s1, string s2) 
 	{
