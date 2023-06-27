3e4c5ed25c718d08ed930d897442b502adc22306
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-06-28 01:24:54 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -7,28 +7,33 @@ class Solution{
 		
 
 	public:
-	int utility(int ind1, int ind2, string s1, string s2, vector<vector<int>> &dp)
-	{
-	    // base case
-	    if(ind1 == 0 || ind2 == 0)
-	        return 0;
-	        
-	   if(dp[ind1][ind2] != -1)
-	        return dp[ind1][ind2];
-	       
-	    // char match
-	    if(s1[ind1 - 1] == s2[ind2 - 1])
-	        return dp[ind1][ind2] = 1+utility(ind1-1, ind2-1, s1, s2, dp);
-	   
-	   // char not matching    
-	    return dp[ind1][ind2] = max(utility(ind1-1, ind2, s1, s2, dp), utility(ind1, ind2-1, s1, s2, dp));
-	}
 	int lcs(string s1, string s2)
 	{
 	    int n = s1.length();
         int m = s2.length();
-        vector<vector<int>>dp(n+1, vector<int>(m+1, ));
-        return utility(n, m, s1, s2, dp);
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        // base case
+        for(int i=0; i<=n; i++)
+        {
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<=m; j++)
+        {
+            dp[0][j] = 0;
+        }
+        
+        // explore all paths
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
 	}
 	int minOperations(string s1, string s2) 
 	{
