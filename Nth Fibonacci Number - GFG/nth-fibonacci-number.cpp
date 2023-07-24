15e55b814ebb914a0a1c92bfaa8bfeac41c04a8c
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-07-24 22:09:55 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -5,30 +5,43 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
+
 class Solution {
   public:
-    int mod = 1000000007;
-    long long int fib(long long int n, vector<long long int> &dp)
-    {
-        if(n<=1)
-            return n;
-        if(dp[n] != -1)
-            return dp[n];
-        return dp[n] = (fib(n-1, dp) + fib(n-2, dp))%mod;
-    }
-    long long int nthFibonacci(long long int n){
-        vector<long long int>dp(n+1, -1);
-        long long int ans = fib(n, dp);
-        return ans;
+    int mod = 1e9+7;
+    // int utility(int n, vector<int> &dp)
+    // {
+    //      if(n==0 || n==1)
+    //         return n;
+        
+    //     if(dp[n] != -1)
+    //         return dp[n];
+        
+    //     dp[n] = (utility(n-1, dp)%mod + utility(n-2, dp)%mod)%mod;
+        
+    //     return dp[n];
+    // }
+    int nthFibonacci(int n){
+        // code here
+        long long prev2 = 0;
+        long long prev1 = 1;
+        for(int i=2; i<=n; i++)
+        {
+            long long curr = (prev1%mod + prev2%mod)%mod;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 };
+
 
 //{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        long long int n;
+        int n;
         cin >> n;
         Solution ob;
         cout << ob.nthFibonacci(n) << endl;
