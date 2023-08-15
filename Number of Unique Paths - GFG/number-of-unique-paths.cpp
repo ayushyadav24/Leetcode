3e8fbebdaa4d56b4711dbd9461ed7590c56ebdb5
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-08-15 13:16:06 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -8,29 +8,48 @@ using namespace std;
 
 class Solution
 {
-    private:
-    int utility(int i, int j,  vector<vector<int>> &dp)
-    {
-        if(i < 0 || j < 0)
-            return 0;
+    // private:
+    // int utility(int i, int j,  vector<vector<int>> &dp)
+    // {
+    //     if(i < 0 || j < 0)
+    //         return 0;
             
-        if(i == 0 && j == 0)
-            return 1;
+    //     if(i == 0 && j == 0)
+    //         return 1;
             
-        if(dp[i][j] != -1)
-            return dp[i][j];
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
         
-        int up = utility(i-1, j, dp);
-        int left = utility(i, j-1, dp);
+    //     int up = utility(i-1, j, dp);
+    //     int left = utility(i, j-1, dp);
         
-        return dp[i][j] =  (up + left);
-    }
+    //     return dp[i][j] =  (up + left);
+    // }
     public:
     //Function to find total number of unique paths.
     int NumberOfPath(int n, int m)
     {
-        vector<vector<int>>dp(n, vector<int>(m, -1));
-        return utility(n-1, m-1, dp);
+        vector<vector<int>>dp(n, vector<int>(m,0));
+        dp[0][0] = 1;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(i == 0 && j==0)
+                    dp[i][j] = 1;
+                else
+                {
+                    int up=0;
+                    int left=0;
+                    if(i > 0)
+                        up = dp[i-1][j];
+                    if(j > 0)
+                        left = dp[i][j-1];
+                    dp[i][j] = (up + left);   
+                }
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
 
