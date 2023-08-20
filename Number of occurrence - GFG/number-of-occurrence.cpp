f656c01531a0f28538638025ddf572a315a27f53
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-08-20 23:49:24 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,20 +10,15 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	int Count(int arr[], int n, int tar, int i)
-	{
-	    int cnt=0;
-	    if(i==n)
-	        return cnt;
-	        
-	   if(arr[i] == tar)
-	        cnt++;
-	        
-	  return cnt + Count(arr, n, tar, i+1);
-	}
 	int count(int arr[], int n, int x) {
 	    // code here
-	   return Count(arr, n, x, 0);
+	    int count = 0;
+	    for(int i=0; i<n; i++)
+	    {
+	        if(arr[i] == x)
+	            count++;
+	    }
+	    return count;
 	}
 };
 
