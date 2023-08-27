27c0f8aabaf7e55e0029b583bf72ccb4080de6af
ayushyadav24 <89244133+ayushyadav24@users.noreply.github.com>
2023-08-28 00:01:00 +0530

    Added solution - LeetHub

---
+++ Magical String[Duplicate Problem] - GFG/magical-string-duplicate-problem.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    string magicalString(string S){
+        // code here 
+        int n = S.length();
+        string ans="";
+        for(int i=0; i<n; i++)
+        {
+            ans += 'z' - S[i] + 'a'; 
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.magicalString(S) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
